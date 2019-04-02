1b4a31f13d15a8131997ba6633e457efef2a5850
Unknown <dmedina032@west-mec.org>
2019-04-02 16:37:24 -0700

    Finished the code
    Inserted the styles for the buttons and for the print option we disabled the buttons so they would therefore not show up

--- bc_switch.js
+++ bc_switch.js
@@ -33,9 +33,10 @@ function setupStyles() {
     //insert some buttons for the style swicher
     var buttonDIV = document.createElement("div");
     buttonDIV.setAttribute("id", "styleButtons");
+
     var webButton = document.createElement("input");
     webButton.setAttribute("type", "button");
-    webButton.setAttribute("value", "web View");
+    webButton.setAttribute("value", "Web View");
 
 
     var pageButton = document.createElement("input");
@@ -52,9 +53,38 @@ function setupStyles() {
     document.head.appendChild(buttonStyles);
 
     //add style rules to the embedded stylesheet 
-    document.styleSheet[document.styleSheets.length - 1].insetRule(
+    document.styleSheets[document.styleSheets.length - 1].insertRule(
         "divstyleButtons { \
             position: fixed; \
-        }",
-        0);
+        }", 0);
+
+    document.styleSheets[document.styleSheets.length - 1].insertRule(
+        "div#styleButtons input { \
+                      background-color: rgba(68, 94, 186, 0.6); \
+                      border: 3px solid rgba(0, 24, 123, 0.6); \
+                      border-radius: 50%; \
+                      cursor: pointer; \
+                      color: white; \
+                      display: inline-block; \
+                      font-size: 1.2em; \
+                      height: 60px; \
+                      margin: 5px 10px; \
+                      width: 100px; \
+                   }", 1);
+
+    document.styleSheets[document.styleSheets.length - 1].insertRule(
+        "@media print { \
+                         div#styleButtons { \
+                            display: none; \
+                         } \
+                      }", 2);
+
+    //turn the page view style off and on
+    webButton.onclick = function () {
+        pageStyle.disabled = true;
+    }
+
+    pageButton.onclick = function () {
+        pageStyle.disabled = false;
+    }
 }
